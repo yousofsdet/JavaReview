f3027635f58dda97348ee62e2744f687724d46b7
yousofsdet <yusuf_bayer@hotmail.com>
2020-06-24 22:12:24 -0400

    First

--- src/test/java/RebootCamp/PrimeNumber.java
+++ src/test/java/RebootCamp/PrimeNumber.java
@@ -1,4 +1,27 @@
 package RebootCamp;
 
+import java.util.Arrays;
+
 public class PrimeNumber {
-}
+
+    public static void main(String[] args) {
+
+        int prime = 12;
+        boolean a = 9>10 && 10>9;
+
+        String str = new String("Java");
+
+        String str1 = "Java";
+        String str2 = new String(str1);
+
+        System.out.print(str1 == str2);
+
+        str1 ="alal";
+        System.out.println(str1);
+
+
+    }
+
+
+
+    }
